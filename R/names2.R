5576b918e1e1b85ac13f79524c854fef6dc849e2
Michel Lang <michellang@gmail.com>
2018-11-26 09:47:39 +0100

    names2

---
+++ R/names2.R
@@ -0,0 +1,27 @@
+#' @title A type-stable names() replacement
+#'
+#' @description
+#' A simple wrapper around [base::names()].
+#' Returns a vector even if no names attribute is set.
+#' Values `NA` and `""` are treated as missing and
+#' replaced with the value provided in `missing_val`.
+#'
+#' @param x \[any\]\cr
+#'   Object.
+#' @param missing_val \[`atomic(1)`\]\cr
+#'   Value to set for missing names. Default is `NA_character_`.
+#' @return \[`character(length(x))`\].
+#' @export
+#' @examples
+#' x = 1:3
+#' names(x)
+#' names2(x)
+#' names(x)[1:2] = letters[1:2]
+#' names(x)
+#' names2(x, missing_val = "")
+names2 = function(x, missing_val = NA_character_) {
+  n = names(x)
+  if (is.null(n))
+    return(rep.int(missing_val, length(x)))
+  replace(n, is.na(n) | !nzchar(n), missing_val)
+}
---
+++ tests/testthat/test_names2.R
@@ -0,0 +1,13 @@
+context("names2")
+
+test_that("names2", {
+  x = 1:3
+  expect_identical(names2(x), rep.int(NA_character_, 3))
+
+  names(x)[1] = letters[1]
+  expect_identical(names2(x), c("a", rep.int(NA_character_, 2)))
+  expect_identical(names2(x, ""), c("a", rep.int("", 2)))
+
+  names(x) = letters[1:3]
+  expect_identical(names(x), names2(x))
+})
