c67e7d6867e83d11bf03d56f333c0ce3b8218571
Michel Lang <michellang@gmail.com>
2018-12-13 14:52:46 +0100

    use do.call2() stuff in invoke to not f-up match.call in models

--- R/invoke.R
+++ R/invoke.R
@@ -1,18 +1,22 @@
 #' @title Invoke a function call
 #'
 #' @description
-#' An alternative interface for [do.call()], similar to the function in \pkg{purrr}.
-#' It is best practice to pass all arguments named.
+#' An alternative interface for [do.call()], similar to the deprecated function in \pkg{purrr}.
+#' This function tries hard to not evaluate the passed arguments too eagerly which is
+#' important when working with large R objects.
+#'
 #'
 #' @param .f ([function()]): Function to call.
-#' @param .x ([list()]): List of function arguments passed to `.f`.
 #' @param ... : Additional function arguments passed to `.f`.
-#'  These arguments will be concatenated to the arguments provied via `.x`.
+#' @param .args ([list()]): List of function arguments passed to `.f`.
+#'  These arguments will be concatenated to the arguments provided via `...`.
 #' @export
 #' @examples
-#' invoke(mean, .x = list(x = 1:10))
-#' invoke(mean, .x = list(1:10), na.rm = TRUE)
-invoke = function(.f, .x = list(), ...) {
-  envir = parent.frame()
-  do.call(.f, c(as.list(.x), list(...)), envir = envir)
+#' invoke(mean, .args = list(x = 1:10))
+#' invoke(mean, na.rm = TRUE, .args = list(1:10))
+invoke = function(.f, ..., .args = list()) {
+  .f = substitute(.f)
+  ddd = match.call(expand.dots = FALSE)$...
+  expr = as.call(c(list(as.name(.f)), ddd, lapply(substitute(.args)[-1L], identity)))
+  eval.parent(expr, n = 1L)
 }
