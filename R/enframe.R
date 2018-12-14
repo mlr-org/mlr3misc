7a58f0e4a3df918b3777cd5c4bd25bdd551b6b20
Michel Lang <michellang@gmail.com>
2018-12-15 00:16:53 +0100

    fix docs

--- R/enframe.R
+++ R/enframe.R
@@ -4,9 +4,9 @@
 #' Returns a [data.table::data.table()] with two columns:
 #' The names of `x` (or seq along `x` if unnamed) and the values of `x`.
 #'
-#' @param x [vector()].
-#' @param name [character(1)]: Name for the first column with names.
-#' @param value [character(1)]: Name for the second column with values.
+#' @param x (`vector()`).
+#' @param name (`character(1)`): Name for the first column with names.
+#' @param value (`character(1)`): Name for the second column with values.
 #'
 #' @return [data.table::data.table()].
 #' @export
