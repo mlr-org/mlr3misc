0eaa16bac7d41c0bcee72ed6ee6dc9f80c460cbc
Michel Lang <michellang@gmail.com>
2018-12-17 07:33:49 +0100

    str_indent: pass args down to str_collapse

--- R/str_indent.R
+++ R/str_indent.R
@@ -2,11 +2,12 @@
 #'   Initial string, passed to [strwrap()].
 #' @param width (`integer(1)`):
 #'   Width, passed to [strwrap()].
+#' @param ... (any): Additional parameters passed to [str_collapse()] to
+#'   collapse `str`.
 #' @rdname string_helpers
 #' @export
-str_indent = function(initial, str, width = 0.9 * getOption("width")) {
+str_indent = function(initial, str, width = 0.9 * getOption("width"), ...) {
   if (length(str) == 0L)
     str = "-"
-  paste0(strwrap(str, initial = paste0(initial, " "), exdent = 2L, width = width),
-    collapse = "\n")
+  strwrap(str_collapse(str, ...), initial = paste0(initial, " "), exdent = 2L, width = width)
 }
