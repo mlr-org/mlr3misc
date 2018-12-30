2fb83179ac6b0dd8796bc399adadc65feaef6740
Bernd Bischl <bernd_bischl@gmx.net>
2018-12-30 17:21:47 +0100

    add: purrr-detect function 'has_element'

---
+++ R/purrr_detect.R
@@ -0,0 +1,17 @@
+#' Does a list contain an object?
+#'
+#' @description
+#' * NB1: Objects are compared with identity.
+#' * NB2: Only use this on lists with complex objects, for simpler structures there are faster operations.
+#' * NB3: Clones of R6 objects are not detected.
+#'
+#' @inheritParams map
+#' @param .y Object to test for
+#' @export
+has_element <- function(.x, .y) {
+  # actually, fastest option would be to for-loop over the list, and break on detection,
+  # but i guess that we would need to do in C code...
+  any(map_lgl(.x, identical, y = .y))
+}
+
+
--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,1 +1,2 @@
 library(checkmate)
+library(R6)
