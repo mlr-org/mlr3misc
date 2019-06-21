1b1e09d233fd5eb7991666d6be74b8d21a5dbfae
Michel Lang <michellang@gmail.com>
2019-06-21 23:29:22 +0200

    add cross_join (fixes #11)

---
+++ R/cross_join.R
@@ -0,0 +1,21 @@
+#' @title Cross-Join for data.table
+#'
+#' @description
+#' A safe version of [data.table::CJ()] in case a column is called
+#' `sorted` or `unique`.
+#'
+#' @param dots (named `list()`)\cr
+#'   Vectors to cross-join.
+#' @param sorted (`logical(1)`)\cr
+#'   See `data.table::CJ()`.
+#' @param unique (`logical(1)`)\cr
+#'   See [data.table::CJ()].
+#' @return [data.table()].
+#' @examples
+#' cross_join(dots = list(sorted = 1:3, b = letters[1:2]))
+cross_join = function(dots, sorted = TRUE, unique = FALSE) {
+  assert_list(dots, names = "unique")
+  nn = names(dots)
+  tab = invoke(CJ, sorted = sorted, unique = unique, .args = unname(dots))
+  setnames(tab, nn)
+}
---
+++ tests/testthat/test_cross_join.R
@@ -0,0 +1,12 @@
+context("chunk")
+
+test_that("cross_join", {
+  tab = cross_join(list(a = 1:3, b = 1:2))
+  expect_equal(tab, CJ(a = 1:3, b = 1:2))
+
+  tab = cross_join(list(sorted = 1:3, b = 1:2))
+  expect_equal(tab, setnames(CJ(a = 1:3, b = 1:2), "a", "sorted"))
+
+  tab = cross_join(list(unique = 1:3, b = 1:2))
+  expect_equal(tab, setnames(CJ(a = 1:3, b = 1:2), "a", "unique"))
+})
