7bb2de428851fbd34e4ae6db3809a4cb64ef217c
mb706 <mlr.developer@mb706.com>
2019-07-03 13:44:57 +0200

    document and export register_dictionary

--- R/register_dictionary.R
+++ R/register_dictionary.R
@@ -1,4 +1,32 @@
 
+#' @title Add Object to Dictionary at Runtime
+#'
+#' @description
+#' `register_dictionary` registers that a dictionary entry
+#' should be created using the given arguments. `publish_registered_dictionary`
+#' executes these calls. `register_dictionary` can be used during build-time
+#' (i.e. at the top-level of the package), while `publish_registered_dictionary`
+#' should be called in `.onLoad()`.
+#'
+#' @param dict_name (`character(1)`)\cr
+#'   Name of dictionary to register the call to. Must be identical between
+#'   `register_dictionary` and `publish_registered_dictionary` calls.
+#' @param ... (any)\cr
+#'   Arguments of `dict_call` to use when creating dictionary entry.
+#' @export
+#' @examples
+#' \dontrun{
+#' # in package top level code:
+#' register_dictionary("learner", "regr.rpart", LearnerRegrRpart)
+#'
+#' # .onLoad code:
+#' .onLoad = function(libname, pkgname) {
+#'   if (is.null(mlr_learners)) {
+#'     mlr_learners <<- DictionaryLearner$new()
+#'   }
+#'   publish_registered_dictionary("learner", mlr_learners$add)
+#' }
+#' }
 register_dictionary = function(dict_name, ...) {
 
   m = match.call()
@@ -15,6 +43,13 @@ register_dictionary = function(dict_name, ...) {
   assign(registry_name, registry, targetenv)
 }
 
+#' @rdname register_dictionary
+#' @param dict_call (`function`)\cr
+#'   Function to call for each registered dictionary entry.
+#' @param targetenv (`environment`)\cr
+#'   Environment in which `register_dictionary` was executed, usually the
+#'   package's namespace.
+#' @export
 publish_registered_dictionary = function(dict_name, dict_call, targetenv = parent.env(parent.frame())) {
   registry_name = sprintf(".registry_%s", dict_name)
 
