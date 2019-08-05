49a04a5911ee9dfd3934f79473641e34da6c767d
Michel Lang <michellang@gmail.com>
2019-08-06 01:51:58 +0200

    added tests for set_names() + set_colnames()

---
+++ tests/testthat/test_set_names.R
@@ -0,0 +1,26 @@
+context("set_names")
+
+test_that("set_names", {
+  x = 1:3
+  expect_names(names(x), "unnamed")
+
+  x = set_names(x, letters[1:3])
+  expect_set_equal(names(x), letters[1:3])
+
+  x = set_names(x, toupper)
+  expect_set_equal(names(x), LETTERS[1:3])
+
+  x = letters[1:3]
+  x = set_names(x)
+  expect_equal(unname(x), names(x))
+})
+
+test_that("set_col_names", {
+  x = iris[, 1:3]
+
+  x = set_col_names(x, letters[1:3])
+  expect_set_equal(names(x), letters[1:3])
+
+  x = set_col_names(x, toupper)
+  expect_set_equal(names(x), LETTERS[1:3])
+})
