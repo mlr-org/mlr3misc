89bc49105fc41c5920d1c0903c26cba7686f78db
Michel Lang <michellang@gmail.com>
2019-09-18 20:53:10 +0200

    added test for which_max

--- tests/testthat/test_which_max.R
+++ tests/testthat/test_which_max.R
@@ -1,6 +1,6 @@
-context("which_min")
+context("which_max")
 
-test_that("which_min", {
+test_that("which_max", {
   expect_identical(which_min(c(1, 9), ties_method = "first"), 1L)
   expect_identical(which_min(c(1, NA, 9), ties_method = "first", na_rm = TRUE), 1L)
   expect_identical(which_min(c(9, 1), ties_method = "first"), 2L)
@@ -30,4 +30,15 @@ test_that("which_min", {
   expect_identical(which_max(NA_integer_), NA_integer_)
   expect_identical(which_min(NA_real_), NA_integer_)
   expect_identical(which_max(NA_real_), NA_integer_)
+})
+
+test_that("na_rm", {
+  expect_equal(which_max(NA_integer_, na_rm = TRUE), integer())
+  expect_equal(which_max(NA_integer_, na_rm = FALSE), NA_integer_)
+  expect_equal(which_max(c(1L, NA_integer_), na_rm = FALSE), NA_integer_)
+  expect_equal(which_max(c(1L, NA_integer_), na_rm = TRUE), 1L)
+  expect_equal(which_max(NA_real_, na_rm = TRUE), integer())
+  expect_equal(which_max(NA_real_, na_rm = FALSE), NA_integer_)
+  expect_equal(which_max(c(1, NA_real_), na_rm = FALSE), NA_integer_)
+  expect_equal(which_max(c(1, NA_real_), na_rm = TRUE), 1L)
 })
