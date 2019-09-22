a40ac0a3587fa1670fb0dd89e1a9ab2b8be72a8b
Michel Lang <michellang@gmail.com>
2019-09-22 22:38:31 +0200

    add cite rd macro

---
+++ R/cite_bib.R
@@ -0,0 +1,44 @@
+#' @title Cite using a Bibtex File
+#'
+#' @description
+#' This function is called by the provided Rd macro `\\cite{pkg}{key}`:
+#'
+#' * Parses the bibtex file `references.bib` in the root directory of package `package` using [bibtex::read.bib()].
+#' * Extracts the entry with key `key`.
+#' * Converts to Rd with [tools::toRd()].
+#'
+#' @param package (`character(1)`)\cr
+#'   Package to read the bibtex file from.
+#' @param key (`character(1)`)\cr
+#'   Entry of the bibtex file.
+#'
+#' @return (`character(1)`) Bibentry formated as Rd.
+#'
+#' @export
+#' @examples
+#' # examplary bibtex file
+#' path = system.file("references.bib", package = "mlr3misc")
+#' cat(readLines(path), sep = "\n")
+#'
+#' # return value: raw rd
+#' cite_bib("mlr3misc", "mlr")
+cite_bib = function(package, key) {
+  assert_string(package)
+  assert_string(key)
+
+  require_namespaces("bibtex")
+  path = system.file("references.bib", package = package)
+  if (!file.exists(path)) {
+    warningf("Bibtex file '%s' for package '%s' does not exist", path, package)
+    return(sprintf("bibtex:%s", key))
+  }
+
+  bib = bibtex::read.bib(path)
+
+  if (key %nin% names(bib)) {
+    warningf("Key '%s' not found in references.bib of package '%s'", key, package)
+    return(sprintf("bibtex:%s", key))
+  }
+
+  tools::toRd(bib[[key]])
+}
