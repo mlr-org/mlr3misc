0beab0ee94b650dd7a3e9a21f9925488c9ae15c0
Michel Lang <michellang@gmail.com>
2020-02-06 20:32:54 +0100

    add open_help() function

---
+++ R/open_help.R
@@ -0,0 +1,18 @@
+#' @title Opens a Manual Page
+#'
+#' @description
+#' Simply opens a manual page specified in "package::topic" syntax.
+#'
+#' @param man :: `character(1)`\cr
+#'   Manual page to open in "package::topic" syntax.
+#' @return Nothing.
+#' @export
+open_help = function(man) {
+  if (!test_string(man)) {
+    message("No help available")
+    return(invisible())
+  }
+
+  parts = strsplit(man, split = "::", fixed = TRUE)[[1L]]
+  match.fun("help")(parts[2L], parts[1L])
+}
