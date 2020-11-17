29479faebb7ef7a97a25c24ee7b404dd298bc52d
Stefan Coors <stefan.coors@gmx.net>
2020-11-17 16:07:51 +0100

    added multioutput formula

--- tests/testthat/test_formulate.R
+++ tests/testthat/test_formulate.R
@@ -7,3 +7,13 @@ test_that("formulate", {
   expect_set_equal(x$lhs, "Species")
   expect_set_equal(x$rhs, c("Sepal.Width", "Petal.Length"))
 })
+
+test_that("formulate_multioutput", {
+  f = formulate(c("Pepal.Width", "Petal.Length"), c("Sepal.Width", "Sepal.Length"))
+  expect_formula(f)
+  expect_null(environment(f))
+
+  x = extract_vars(f)
+  expect_set_equal(x$lhs, c("Pepal.Width", "Petal.Length"))
+  expect_set_equal(x$rhs, c("Sepal.Width", "Sepal.Length"))
+})
