7bd6199f8a29aa9ab1500c941b13d99ce3d4dfea
Michel Lang <michellang@gmail.com>
2021-03-15 11:13:42 +0100

    make formulate more robust

--- R/formulate.R
+++ R/formulate.R
@@ -22,6 +22,8 @@
 #' formulate(rhs = c("Sepal.Length", "Sepal.Width"))
 formulate = function(lhs = character(), rhs = character(), env = NULL, quote = "right") {
   assert_subset(quote, choices = c("left", "right"))
+  lhs = as.character(lhs)
+  rhs = as.character(rhs)
 
   if (length(lhs) == 0L) {
     lhs = ""
@@ -31,7 +33,7 @@ formulate = function(lhs = character(), rhs = character(), env = NULL, quote = "
 
   if (length(rhs) == 0L) {
     rhs = "1"
-  } else if ("right" %in% quote) {
+  } else if ("right" %in% quote && !identical(rhs, "1")) {
     rhs = paste0("`", rhs, "`")
   }
 
