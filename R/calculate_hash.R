ab0f007dcd23c0b0b7399e616c49f441d63772a7
Michel Lang <michellang@gmail.com>
2021-07-13 09:56:17 +0200

    cleanup

--- R/calculate_hash.R
+++ R/calculate_hash.R
@@ -2,12 +2,16 @@
 #'
 #' @description
 #' Calls [digest::digest()] to calculate the hash for all objects provided.
+#'
 #' The following operations are performed to make hashing more robust:
 #' * If an object is a [function()], the formals and the body are hashed separately.
 #'   This ensures that the bytecode or parent environment are not be included
 #'   in the hash.
 #' * If an object is a [data.table::data.table()], the data.table is converted to a
 #'   regular list. This ensures that keys and indices are not included in the hash.
+#'
+#' Note that this only applies to top level objects, these transformations are not done
+#' recursively.
 #'
 #' @param ... (any)\cr
 #'   Objects to hash.
@@ -16,10 +20,13 @@
 #' @examples
 #' calculate_hash(iris, 1, "a")
 calculate_hash = function(...) {
-  dots = list(...)
-  dots = map_if(dots, is.function, function(fun) {
-    list(formals(fun), as.character(body(fun)))
-  })
-  dots = map_if(dots, is.data.table, as.list)
-  digest(dots, algo = "xxhash64")
+  digest(lapply(list(...), function(x) {
+    if (is.function(x)) {
+      list(formals(fun), as.character(body(fun)))
+    } else if (is.data.table(x)) {
+      as.list(x)
+    } else {
+      x
+    }
+  }), algo = "xxhash64")
 }
