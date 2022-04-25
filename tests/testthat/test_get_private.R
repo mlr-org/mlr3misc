603176ce243632c6dc97dc293b041ed50ca1fde5
Sebastian Fischer <sebf.fischer@gmail.com>
2022-04-25 14:06:06 +0200

    document and add tests

--- tests/testthat/test_get_private.R
+++ tests/testthat/test_get_private.R
@@ -8,3 +8,21 @@ test_that("get_private<- works", {
   get_private(a, "y") = 314L
   expect_true(get_private(a)$y == 314L)
 })
+
+test_that("get_private<- checks for correct input", {
+  a = R6Class("A",
+    private = list(
+      y = 123
+    )
+  )$new()
+
+  expect_error({
+    get_private(a, c("x", "y")) = 314L
+  })
+  expect_error({
+    get_private(a, character(0)) = 314L
+  })
+  expect_error({
+    get_private(a, NULL) = 314L
+  })
+})
