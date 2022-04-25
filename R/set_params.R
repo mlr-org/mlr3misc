e95bb9f826edb396a1299293f4bfaf76bdf636ae
Sebastian Fischer <sebf.fischer@gmail.com>
2022-04-25 20:53:55 +0200

    feat: Convenience function set_params

---
+++ R/set_params.R
@@ -0,0 +1,37 @@
+#' @title Modify Values of a Parameter Set
+#'
+#' @description Convenience function to modfiy (or overwrite) the values of a [paradox::ParamSet].
+#'
+#' @param .ps ([paradox::ParamSet])\cr
+#'   The parameter set whose values are changed.
+#' @param ... (any)
+#'   Named parameter values.
+#' @param .values (`list()`)
+#'   Named list with parameter values.
+#' @param .insert (`logical(1)`)\cr
+#'  Whether to insert the values (old values are being kept, if not overwritten), or to discard the
+#'  old values. Is TRUE by default.
+#'
+#' @export
+#' @examples
+#' param_set = paradox::ps(a = paradox::p_dbl(), b = paradox::p_dbl())
+#' param_set$values$a = 0
+#' set_params(param_set, a = 1, .values = list(b = 2), .insert = TRUE)
+#' set_params(param_set, a = 3, .insert = FALSE)
+#' set_params(param_set, b = 4, .insert = TRUE)
+set_params = function(.ps, ..., .values = list(), .insert = TRUE) {
+  dots = list(...)
+  assert_named(dots)
+  assert_list(.values)
+  if (length(.values)) {
+    assert_names(names(.values))
+  }
+  assert_disjunct(names(dots), names(.values))
+  new_values = c(dots, .values)
+  if (.insert) {
+    .ps$values = insert_named(.ps$values, new_values)
+  } else {
+    .ps$values = new_values
+  }
+  return(.ps)
+}
