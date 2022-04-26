77b2a860eae89d7d4761940fbf2e613fdafb0572
Sebastian Fischer <sebf.fischer@gmail.com>
2022-04-26 14:15:43 +0200

    Assign private (#69)

--- R/get_private.R
+++ R/get_private.R
@@ -19,3 +19,33 @@ get_private = function(x) {
 
   x[[".__enclos_env__"]][["private"]]
 }
+
+#' @title Assign Value to Private Field
+#'
+#' @description
+#' Convenience function to assign a value to a private field of an [R6] instance.
+#'
+#' @param x (any)\cr
+#'   Object whose private field should be modified.
+#' @param which (character(1))\cr
+#'   Private field that is being modified.
+#' @param value (any)\cr
+#'   Value to assign to the private field.
+#'
+#' @return The R6 instance x, modified in-place. If it is not an R6 instance, NULL is returned.
+#' @export
+#' @examples
+#' library(R6)
+#' item = R6Class("Item", private = list(x = 1))$new()
+#' get_private(item)$x
+#' get_private(item, "x") = 2L
+#' get_private(item)$x
+`get_private<-` = function(x, which, value) {
+  if (!inherits(x, "R6")) {
+    return(NULL)
+  }
+  assert_character(which, len = 1L, any.missing = FALSE, min.chars = 1L)
+
+  x[[".__enclos_env__"]][["private"]][[which]] = value
+  x
+}
