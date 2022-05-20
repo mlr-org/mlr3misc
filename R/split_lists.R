358312fde2725079f0c82e236bb25519ed3a1be6
Sebastian Fischer <sebf.fischer@gmail.com>
2022-05-20 13:31:01 +0200

    fix: improve fix_list

--- R/split_lists.R
+++ R/split_lists.R
@@ -2,10 +2,11 @@
 #'
 #' @description
 #' Named lists can be split into multiple lists by applying regexes to its names.
+#' If the list is unnamed, all names are treated to be `""` for consistency.
 #'
 #' @param x (named `list()`)\cr
 #'   The list that will be split.
-#' @param patterns (`list()`)\cr
+#' @param patterns (`character()`)\cr
 #'   A list containing various regex patterns. If it is named, the output inherits those names.
 #' @param ... (any)\cr
 #'   Additional arguments to `grepl()`.
@@ -16,15 +17,18 @@
 #' @export
 split_list = function(x, patterns, ...) {
   assert_list(x)
-  assert_list(patterns)
-  nms = names(x) %??% ""
-  out = map(
-    patterns,
-    function(pattern) {
-      x[grepl(pattern, nms, ...)]
-    }
-  )
-  set_names(out, names(patterns))
+  assert_character(patterns, any.missing = FALSE)
+  if (length(x)) {
+    nms = names(x) %??% ""
+    out = map(
+      patterns,
+      function(pattern) {
+        x[grepl(pattern, nms, ...)]
+      }
+    )
+  } else {
+    out = replicate(length(patterns), list())
+  }
 
-  out
+  set_names(out, names(patterns))
 }
