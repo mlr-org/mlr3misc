38a14addd11efa20ee26cd226d752d55048c7e91
Sebastian Fischer <sebf.fischer@gmail.com>
2022-05-20 12:26:53 +0200

    feat: Split lists by regexing over names

---
+++ R/split_lists.R
@@ -0,0 +1,28 @@
+#' @title Splits a list based on names
+#' @description
+#' Named lists can be split into multiple lists by applying regexes to its names.
+#'
+#' @param x (named `list()`)\cr
+#'   The list that will be split.
+#' @param patterns (`list()`)\cr
+#'   A list containing various regex patterns. If it is named, the output inherits those names.
+#' @param ... (any)\cr
+#'   Additional arguments to `grepl()`.
+#'
+#'@return
+#' A (possibly named) `list()` of subsets of x.
+#'
+#' @export
+split_list = function(x, patterns, ...) {
+  assert_list(x)
+  assert_true(!is.null(names(x)))
+  out = map(
+    patterns,
+    function(pattern) {
+      x[grepl(pattern, names(x), ...)]
+    }
+  )
+  set_names(out, names(patterns))
+
+  out
+}
