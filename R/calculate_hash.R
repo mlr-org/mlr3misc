bd52c02bb5c8fa936fb81df711c95c4c23ec65cf
Sebastian Fischer <sebf.fischer@gmail.com>
2024-01-24 09:19:02 +0100

    better docu

--- R/calculate_hash.R
+++ R/calculate_hash.R
@@ -1,19 +1,7 @@
 #' @title Calculate a Hash for Multiple Objects
 #'
 #' @description
-#' Calls [digest::digest()] to calculate the hash for all objects provided.
-#' By specifying methods for the [`hash_input`] generic, you can control which information of an object
-#' is used to calculate the hash.
-#'
-#' Methods exist for:
-#' * If an object is a [function()], the formals and the body are hashed separately.
-#'   This ensures that the bytecode or parent environment are not be included
-#'   in the hash.
-#' * If an object is a [data.table::data.table()], the data.table is converted to a
-#'   regular list. This ensures that keys and indices are not included in the hash.
-#'
-#' Note that this only applies to top level objects, these transformations are not done
-#' recursively.
+#' Calls [digest::digest()] using the 'xxhash64' algorithm after applying [`hash_input()`].
 #'
 #' @param ... (any)\cr
 #'   Objects to hash.
@@ -28,25 +16,37 @@ calculate_hash = function(...) {
 
 #' Hash Input
 #'
-#' Returns the information of an object to be used to calculate its hash.
+#' Returns the part of an object to be used to calculate its hash.
+#'
+#' Methods exist for:
 #' @param x (any)\cr
-#'   Object that is part of the object to be hashed.
+#'   Object for which to retrieve the hash input.
 #' @export
 hash_input = function(x) {
   UseMethod("hash_input")
 }
 
+#' @describeIn hash_input
+#' The formals and the body are hashed separately.
+#' This ensures that the bytecode or parent environment are not be included
+#' in the hash.
 #' @export
 hash_input.function = function(x) {
   list(formals(x), as.character(body(x)))
 }
 
+#' @describeIn hash_input
+#' The data.table is converted to a regular list and.
+#' This ensures that keys and indices are not included in the hash.
+#' Then, [`hash_input`] is applied to each element of the list.
 #' @export
 #' @method hash_input data.table
 hash_input.data.table = function(x) {
   lapply(as.list(x), hash_input)
 }
 
+#' @describeIn hash_input
+#' Returns the object as is.
 #' @export
 hash_input.default = function(x) {
   x
