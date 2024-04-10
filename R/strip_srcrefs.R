8621269867d7666c588d34ebb636fc974112c184
be-marc <marcbecker@posteo.de>
2024-04-10 15:37:42 +0200

    fix: docs

--- R/strip_srcrefs.R
+++ R/strip_srcrefs.R
@@ -2,7 +2,7 @@
 #'
 #' @description
 #' Source references can make objects unexpectedly large and are undesireable in many situations.
-#' As {renv} installs packages with the `--with-keep.source` option, we sometimes need to remove source references
+#' As \CRANpkg{renv} installs packages with the `--with-keep.source` option, we sometimes need to remove source references
 #' from objects.
 #' Methods should remove source references from the input, but should otherwise leave the input unchanged.
 #'
