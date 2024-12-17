b10538660a6a1eeba8c2fe8f1298715edd3a7e7a
kenomersmannLaptop <advieser@gmail.com>
2024-12-17 16:24:40 +0100

    add basic tests

--- tests/testthat/test_check_operators.R
+++ tests/testthat/test_check_operators.R
@@ -1,3 +1,13 @@
 test_that("check operators", {
+  # AND operator
+  expect_true(TRUE %check&&% TRUE)
+  expect_error(TRUE %check&&% "error", "error")
+  expect_error("error" %check&&% TRUE, "error")
+  expect_error("error1" %check&&% "error2", "error1 and error2")
+
+  expect_true(TRUE %check||% TRUE)
+  expect_true(TRUE %check||% "error")
+  expect_true("error" %check||% TRUE)
+  expect_error("error1" %check||% "error2", "error1 or error2")
 
 })
