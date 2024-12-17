e88768f7304c7b07b6b9e268744938978b06cf35
kenomersmannLaptop <advieser@gmail.com>
2024-12-17 16:21:29 +0100

    ran document()

--- R/check_operators.R
+++ R/check_operators.R
@@ -1,26 +1,26 @@
 #' @title Logical Check Operators
 #'
 #' @description
-#' AND and OR operators for [`checkmate`] `check_*`-functions.
+#' Logical AND and OR operators for `check_*`-functions from [`checkmate`].
 #'
-#' @param lhs,rhs (`check_*`-functions)\cr
+#' @param lhs,rhs (`function()`)\cr
 #'   `check_*`-functions that return either `TRUE` or an error message.
 #'
-#' @name check_ops
+#' @name check_operators
 #' @examples
 #' check_numeric(x) %check&&% check_true(all(x < 0))
 #' check_numeric(x) %check||% check_character(x)
 NULL
 
 #' @export
-#' @rdname check_ops
+#' @rdname check_operators
 `%check&&%` = function(lhs, rhs) {
   if (!isTRUE(lhs) && !isTRUE(rhs)) return(paste0(lhs, ", and ", rhs))
   if (isTRUE(lhs)) rhs else lhs
 }
 
 #' @export
-#' @rdname check_ops
+#' @rdname check_operators
 `%check||%` = function(lhs, rhs) {
   if (!isTRUE(lhs) && !isTRUE(rhs)) return(paste0(lhs, ", or ", rhs))
   TRUE
