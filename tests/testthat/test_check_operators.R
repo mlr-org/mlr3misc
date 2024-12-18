8da37e1098d006bc301e5ef7a956787cd581b2e0
kenomersmannPC <advieser@gmail.com>
2024-12-18 12:29:51 +0100

    corrected tests

--- tests/testthat/test_check_operators.R
+++ tests/testthat/test_check_operators.R
@@ -1,13 +1,13 @@
 test_that("check operators", {
   # AND operator
   expect_true(TRUE %check&&% TRUE)
-  expect_error(TRUE %check&&% "error", "error")
-  expect_error("error" %check&&% TRUE, "error")
-  expect_error("error1" %check&&% "error2", "error1 and error2")
+  expect_equal(TRUE %check&&% "error", "error")
+  expect_equal("error" %check&&% TRUE, "error")
+  expect_equal("error1" %check&&% "error2", "error1, and error2")
 
+  # OR operator
   expect_true(TRUE %check||% TRUE)
   expect_true(TRUE %check||% "error")
   expect_true("error" %check||% TRUE)
-  expect_error("error1" %check||% "error2", "error1 or error2")
-
+  expect_equal("error1" %check||% "error2", "error1, or error2")
 })
