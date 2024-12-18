ee257d03724a23ab7ab3cc2ed16d0eeb960712bc
kenomersmannPC <advieser@gmail.com>
2024-12-18 12:36:44 +0100

    docs: small changes

--- R/check_operators.R
+++ R/check_operators.R
@@ -4,7 +4,7 @@
 #' Logical AND and OR operators for `check_*`-functions from [`checkmate`][`checkmate::checkmate`].
 #'
 #' @param lhs,rhs (`function()`)\cr
-#'   `check_*`-functions that return either `TRUE` or an error message.
+#'   `check_*`-functions that return either `TRUE` or an error message as a `character(1)`.
 #'
 #' @return Either `TRUE` or a `character(1)`.
 #'
@@ -12,7 +12,7 @@
 #' @examples
 #' library(checkmate)
 #'
-#' x = c(0, 1, 2, 3)#
+#' x = c(0, 1, 2, 3)
 #' check_numeric(x) %check&&% check_names(names(x), "unnamed")  # is TRUE
 #' check_numeric(x) %check&&% check_true(all(x < 0))  # fails
 #'
